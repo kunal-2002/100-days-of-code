6e578220baf13d2acac030778c3737c1cb7bd19f
Kunal Kathpal <kkunal2103@outlook.com>
2023-03-29 22:50:25 +0530

    Day27

---
+++ Day_027.cpp
@@ -0,0 +1,24 @@
+/**
+ *Problem Statement:    a program to find the double of the given number without using arithmetic operator.
+ *Author: Kunal Kathpal (https://github.com/kunal-2002)
+ */
+	
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Enter number of test cases:\t";
+	int T;
+	cin>>T;
+	
+	while(T--){
+		int n;
+		cout<<"Enter a number:\t";
+		
+		cin>>n;
+		cout<<(n<<1)<<endl;
+		      
+	}
+	return 0;
+}
