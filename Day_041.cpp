bf0d3e6095a7c7eb1eda4c35ac9b1952994b0fe1
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-15 20:01:33 +0530

    Day41

--- Day_041.cpp
+++ Day_041.cpp
@@ -7,6 +7,21 @@
 
 using namespace std;
 
+bool checking(char* A, char* B){
+	if(*A == '\0' && *B == '\0')
+		return true;
+	if(*A == '*' && *(A+1) != '\0' && *B == '\0')
+		return false;
+	if(*A == '?' || *A == *B)
+		return checking(A+1,B+1);
+	if(*A == '*')
+		return checking(A+1,B) || checking(A, B+1);
+	return false;
+}
+
+void testing(char* A, char* B){
+	checking(A,B)?puts("Yes"):puts("No");
+}
 
 int main(){
 	cout<<"Enter number of test cases:\t";
@@ -14,6 +29,15 @@ int main(){
 	cin>>T;
 	
 	while(T--){
+		char A[20], B[20];
+		cout<<"Enter first string with wild characters:\t";
+		cin>>A;
+		cout<<"Enter second string without wild characters:\t";
+		cin>>B;
+		
+		testing(A,B);
+		
+		cout<<endl;
 		
 	}
 	return 0;
