7f9a8e61dada3b79ebbce33560d410ddeecd61a0
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-15 19:20:46 +0530

    Day43

--- Day_043.cpp
+++ Day_043.cpp
@@ -13,7 +13,33 @@ int main(){
 	cin>>T;
 	
 	while(T--){
+		int size;
 		
+		cout<<"Enter size of array:\n";
+		cin>>size;
+		
+		int arr[size];
+		
+		cout<<"Enter elements:\n";
+		for(int i=0;i<size;i++)
+			cin>>arr[i];
+		
+		int odd = 0, even = 0;
+		
+		for(int i = 0; i<size; i++){
+			if(arr[i]%2 == 0)
+				even++;
+			if(arr[i]%2 == 1)
+				odd++;
+		}
+		if(odd == size)
+			cout<<"Odd";
+		else if(even == size)
+			cout<<"Even";
+		else
+			cout<<"Mixed";
+		
+		cout<<endl;
 	}
 	return 0;
 }
