181c26e3ac2e7d3c5240869d3552cb4de13a6f04
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-23 02:15:09 +0530

    Day51

--- Day_051.cpp
+++ Day_051.cpp
@@ -1,6 +1,5 @@
 /**
- *Problem Statement:    Given an integer array of size N. Write Program to find sum of positive 
-						square elements in the array.
+ *Problem Statement:    Given an integer array of size N, write a program to sort the array.
  *Author: Kunal Kathpal (https://github.com/kunal-2002)
  */
 	
@@ -8,38 +7,30 @@
 
 using namespace std;
 
-int solve(int arr[], int n){
-	if(n==0 || n==1)
-		return n;
+//Quick Sort Method
+
+int partition(int arr[], int low, int high){
+	int pivot = arr[high];
+	int i = (low - 1);
 	
-	int temp[n];
-	
-	int j=0;
-	
-	for(int i=0;i<n-1;i++){
-		if(arr[i] != arr[i+1])
-			temp[j++] = arr[i];
+	for(int j = low; j <= high; j++){
+		if(arr[j] < pivot){
+			i++;
+			swap(arr[i], arr[j]);
+		}
 	}
 	
-	temp[j++] = arr[n-1];
-	
-	for(int i=0;i<j;i++){
-		arr[i] = temp[i];
-	}
-	
-	return j;
-	
+	swap(arr[i+1], arr[high]);
+	return (i+1);
 }
 
-long long int SumOfSquares(int arr[], int n){
-	long long int sum = 0;
-	
-	for(int i=0; i<n; i++){
+void quickSort(int arr[], int low, int high){
+	if(low<high){
+		int pi = partition(arr, low, high);
 		
-		sum = sum + arr[i]*arr[i];
+		quickSort(arr, low, pi-1);
+		quickSort(arr, pi+1, high);
 	}
-	
-	return sum;
 }
 
 int main(){
@@ -54,17 +45,14 @@ int main(){
 		
 		for(int i=0; i<n; i++){
 			cin>>arr[i];
-			if(arr[i]<0){
-				arr[i] = -arr[i];
-			}
 		}
 		
-		sort(arr, arr+n);
+		quickSort(arr, 0, n-1);
 		
-		n = solve(arr, n);
+		for(int i = 0; i<n; i++)
+			cout<< arr[i]<<" ";
 		
-		cout<<SumOfSquares(arr, n)<<endl;
-		
+		cout<<endl;
 	}
 	return 0;
 }
