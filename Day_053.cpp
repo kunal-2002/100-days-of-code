072a18600df9b285384ce617f641d324d308900e
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-25 11:37:24 +0530

    Day53

--- Day_053.cpp
+++ Day_053.cpp
@@ -8,12 +8,32 @@
 
 using namespace std;
 
+int MaxSubArrProd(int arr[], int n){
+	int ans = arr[0];
+	for(int i = 0;i<n; i++){
+		int prod = arr[i];
+		for(int j = i+1;j<n;j++){
+			ans = max(ans,prod);
+			prod = prod*arr[j];
+		}
+		ans = max(ans, prod);
+	}
+	return ans;
+}
 int main(){
 	cout<<"Enter number of test cases:\t";
 	int T;
 	cin>>T;
 	
 	while(T--){
+		int n;
+		cin>>n;
+		int arr[n];
+		
+		for(int i=0; i<n; i++)
+			cin>>arr[i];
+		
+		cout<<MaxSubArrProd(arr,n)<<endl;
 		
 	}
 	return 0;
