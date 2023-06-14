c3f4a36f379bedd2b42aed3a33232e68bdbbced3
Kunal Kathpal <kkunal2103@outlook.com>
2023-06-15 00:57:19 +0530

    Day100

--- Day_100.cpp
+++ Day_100.cpp
@@ -16,9 +16,47 @@ int main(){
 	cout<<"Enter number of test cases:\t";
 	int T;
 	cin>>T;
-
+	int count=0;
+	int counter=1;
+	vector<int>array(4);
 	while(T--){
-
+		count=1;
+	    // getting an array
+	    for(int j=0;j<4;j++){
+	        cin>>array[j];
+	    }
+	    // sorting an arrray
+	    sort(array.begin(),array.end());
+	    // counting the maximum possible count
+	    for(int j=0;j<4;j++){
+	        for(int l=j+1;l<4;l++){
+	            if(array[j]==array[l]){
+	                counter++;
+	                
+	            }
+	            else{
+	                count=max(counter,count);
+	                counter=1;
+	                break;
+	            }
+	            if(l==3){
+	                count=max(counter,count);
+	                counter=1;
+	            }
+	        }
+	    }
+	    if(count==3){
+	        cout<<1<<endl;
+	    }
+	    else if(count==2){
+	        cout<<2<<endl;
+	    }
+        else if(count==1){
+            cout<<2<<endl;
+        }
+	    else if(count==4){
+	        cout<<0<<endl;
+	    }
 	}
 	return 0;
 }
