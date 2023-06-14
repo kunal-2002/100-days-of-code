6b7a959a53e45d29bcab5ae1e02fc1dff170aa4c
Kunal Kathpal <kkunal2103@outlook.com>
2023-06-14 20:09:24 +0530

    Day93

--- Day_093.cpp
+++ Day_093.cpp
@@ -14,14 +14,32 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+const int maxw = 2005;
 
 int main(){
-	cout<<"Enter number of test cases:\t";
-	int T;
-	cin>>T;
+	int N;
+       cin>>N;
+       string a[N];
+       bitset<maxw>b, q[N];
+       for(int i=0;i<N;i++)
+       {
+              cin>>a[i];
+              for(int j=0;j<N;j++)
+              {
+                     q[i][j] = a[i][j] - '0';
+              }
+              q[i][i] = 0;
+       }
+       int res = 0;
+       for(int i=0;i<N;i++){
+              for(int j=i+1;j<N;j++){
+                     b = q[i]&q[j];
+                     if(q[i][j]==0 && b.count()>=1 ){
+                            res+=2;
+                     }
+              }
+       }
+       cout<<res<<endl;
 
-	while(T--){
-
-	}
 	return 0;
 }
