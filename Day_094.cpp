31da36f751904580b55c17da57e66dc0742ff67c
Kunal Kathpal <kkunal2103@outlook.com>
2023-06-14 22:50:01 +0530

    Day94

--- Day_094.cpp
+++ Day_094.cpp
@@ -13,17 +13,77 @@
  *Author: Kunal Kathpal (https://github.com/kunal-2002)
  */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-	cout<<"Enter number of test cases:\t";
-	int T;
-	cin>>T;
+void dfs(int gnome_id, int remaining_time, const vector<vector<int>>& g, vector<bool>& visited, set<int>& recipients) {
+    visited[gnome_id] = true;
 
-	while(T--){
+    if (remaining_time == 0) {
+        recipients.insert(gnome_id);
+    }
 
-	}
-	return 0;
+    if (remaining_time > 0) {
+        for (int i = 0; i < g.size(); i++) {
+            if (g[gnome_id][i] == 1 && !visited[i]) {
+                dfs(i, remaining_time - 1, g, visited, recipients);
+            }
+        }
+    }
 }
+
+void spread_joke(int N, const vector<vector<int>>& g, const vector<pair<int, int>>& queries) {
+    for (const auto& query : queries) {
+        int k = query.first;
+        int x = query.second;
+
+        vector<bool> visited(N, false);
+        set<int> recipients;
+        dfs(x - 1, k, g, visited, recipients);
+
+        int num_recipients = recipients.size();
+        vector<int> sorted_recipients(recipients.begin(), recipients.end());
+        sort(sorted_recipients.begin(), sorted_recipients.end());
+
+        cout << num_recipients << endl;
+        if (num_recipients > 0) {
+            for (int i = 0; i < num_recipients; i++) {
+                cout << sorted_recipients[i] + 1 << " ";
+            }
+            cout << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<vector<int>> g(N, vector<int>(N));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> g[i][j];
+        }
+    }
+
+    int M;
+    cin >> M;
+
+    vector<pair<int, int>> queries;
+    for (int i = 0; i < M; i++) {
+        int k, x;
+        cin >> k >> x;
+        queries.emplace_back(k, x);
+    }
+
+    spread_joke(N, g, queries);
+
+    return 0;
+}
+
