8852a9f63e20c89c98108da59a7bb71c4d9a114d
Kunal Kathpal <kkunal2103@outlook.com>
2023-06-14 23:01:38 +0530

    Day95

--- Day_095.cpp
+++ Day_095.cpp
@@ -7,9 +7,26 @@
  *Author: Kunal Kathpal (https://github.com/kunal-2002)
  */
 
+#include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long int ll;
+
+bool isPowerOf2(ll n){
+	return(ceil(log2(n)) == floor(log2(n)));
+}
+
+long long binpow(long long a, long long b){
+	long long res = 1;
+	while(b>0){
+		if(b&1)
+			res = res*a;
+		a = a*a;
+		b >>= 1;
+	}
+	return res;
+}
 
 int main(){
 	cout<<"Enter number of test cases:\t";
@@ -17,7 +34,19 @@ int main(){
 	cin>>T;
 
 	while(T--){
-
+		ll n;
+		cin>>n;
+		if(isPowerOf2(n) == true)
+			cout<<"0\n";
+		else{
+			int cnt = 0;
+			ll temp = n;
+			while(isPowerOf2(temp) == false){
+				cnt++;
+				temp = temp - binpow(2, log2(temp));
+			}
+			cout<<cnt<<"\n";
+		}
 	}
 	return 0;
 }
